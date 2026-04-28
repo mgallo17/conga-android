a9e4e5acb3c6d0aaad808f1ec59d410d1ba73495
Matheus Gallo <mgallo17@gmail.com>
2026-04-28 21:08:00 +0000

    fix: import android.view.View em falta no MainActivity

--- app/src/main/java/com/mgallo17/conga/MainActivity.java
+++ app/src/main/java/com/mgallo17/conga/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
